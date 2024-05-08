3d8651251a879cea5e587367abe35b738ed46d0a
Walter-OG <walterog.97@gmail.com>
2024-05-07 21:56:58 -0500

    Se agrega el proyecto

---
+++ main.py
@@ -0,0 +1,16 @@
+# #Pasos para usar pandas
+# #1. importar pandas
+
+# import pandas as pd
+
+# #2. traer datos
+
+# tabla_encicla=pd.read_csv("./data/EstacionesEnCicla_DatosAbiertos2_1.csv")
+
+# print (tabla_encicla)
+
+# tabla_siembras=pd.read_csv("./data/PlanSiembra_dic2019.csv")
+
+# print (tabla_siembras.describe())
+
+
